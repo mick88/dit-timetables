619d89fc6c7ed626cf4e1beb9a522dd5d033c942
michaldabski <contact@michaldabski.com>
2013-07-28 21:20:40 +0100

    style activity

--- src/com/mick88/dittimetable/UnfoldActivity.java
+++ src/com/mick88/dittimetable/UnfoldActivity.java
@@ -1,16 +1,24 @@
 package com.mick88.dittimetable;
 
+import com.mick88.dittimetable.utils.FontApplicator;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class UnfoldActivity extends Activity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 		setContentView(R.layout.activity_unfold);
+		
+		LinearLayout container = (LinearLayout) findViewById(R.id.container);
+		
+		FontApplicator fontApplicator = new FontApplicator(getAssets(), "Roboto-Light.ttf");
+		fontApplicator.applyFont(container);
 	}
 
 }
