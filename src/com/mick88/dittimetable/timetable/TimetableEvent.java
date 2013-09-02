2f464de93832d68926e3390a508899de0ad23fc4
michaldabski <contact@michaldabski.com>
2013-09-02 22:19:28 +0100

    Remove unneeded log messages from TimetableEvent

--- src/com/mick88/dittimetable/timetable/TimetableEvent.java
+++ src/com/mick88/dittimetable/timetable/TimetableEvent.java
@@ -370,11 +370,8 @@ public class TimetableEvent implements Comparable<TimetableEvent>, EventItem, Se
 			return false;
 		}
 		
-		Log.i(logTag, "Event loaded from "+filename);
-		
 		if (parseAdditionalInfo(sb.toString()))
 		{
-			Log.i(logTag, "Timetable successfully loaded from file");
 			return true;
 		}
 		return false;
@@ -388,7 +385,6 @@ public class TimetableEvent implements Comparable<TimetableEvent>, EventItem, Se
 		file.write(buffer);
 		file.flush();
 		file.close();
-		Log.i(logTag, "Timetable saved to "+filename);
 	}
 	
 	/**
@@ -568,7 +564,6 @@ public class TimetableEvent implements Comparable<TimetableEvent>, EventItem, Se
 				}
 			}
 		}
-		Log.d(logTag, "Weeks decoded to "+weeks.size());
 	}
 	
 	/**
