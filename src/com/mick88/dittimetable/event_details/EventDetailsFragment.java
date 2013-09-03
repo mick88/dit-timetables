ec66ed60625662eafbea5e6140aadd3c9159fda8
michaldabski <contact@michaldabski.com>
2013-09-03 21:32:28 +0100

    Add event details fragment

---
+++ src/com/mick88/dittimetable/event_details/EventDetailsFragment.java
@@ -0,0 +1,29 @@
+package com.mick88.dittimetable.event_details;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class EventDetailsFragment extends Fragment
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View getView()
+	{
+		// TODO Auto-generated method stub
+		return super.getView();
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+	}
+}
