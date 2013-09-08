12ecc1f7a7fd890e7d6c5ddefef9fa6edb0052be
michaldabski <contact@michaldabski.com>
2013-09-08 12:22:50 +0100

    implement service to fetch pdf url async

--- src/com/mick88/dittimetable/PdfDownloaderService.java
+++ src/com/mick88/dittimetable/PdfDownloaderService.java
@@ -1,18 +1,51 @@
 package com.mick88.dittimetable;
 
+import com.mick88.dittimetable.timetable.Timetable;
+
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 
 public class PdfDownloaderService extends Service
 {
-	public PdfDownloaderService() {
+	public static final String EXTRA_TIMETABLE = "timetable";
+	
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		downloadPdf((Timetable) intent.getSerializableExtra(EXTRA_TIMETABLE));
+		return START_STICKY;
 	}
-
+	
 	@Override
 	public IBinder onBind(Intent intent)
 	{
 		// TODO: Return the communication channel to the service.
 		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	private void downloadPdf(Timetable timetable)
+	{
+		new AsyncTask<Timetable, Void, Void>()
+		{
+
+			@Override
+			protected Void doInBackground(Timetable... params)
+			{
+				Timetable timetable = params[0];
+				String url = timetable.getPdfUrl();
+				
+				return null;
+			}
+			
+			@Override
+			protected void onPostExecute(Void result) 
+			{
+				stopSelf();
+			}
+			
+		}.execute(timetable);
 	}
 }
