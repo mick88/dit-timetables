5804c0911bd934b8e8f7579472bc40421bc7b2fa
michaldabski <contact@michaldabski.com>
2013-11-01 19:24:05 +0000

    Add methods to save and load timetable

--- src/com/mick88/dittimetable/DatabaseHelper.java
+++ src/com/mick88/dittimetable/DatabaseHelper.java
@@ -1,5 +1,7 @@
 package com.mick88.dittimetable;
 
+import java.util.List;
+
 import android.content.Context;
 
 import com.michaldabski.msqlite.MSQLiteOpenHelper;
@@ -10,10 +12,23 @@ public class DatabaseHelper extends MSQLiteOpenHelper
 	private static final String name = "timetables.db";
 	private static final int version = 1;
 	
-	public DatabaseHelper(Context context, Class<?>[] trackedClasses) {
+	public DatabaseHelper(Context context) {
 		super(context, name, null, version, new Class<?>[]{
 				Timetable.class,
 		});
 	}
 	
+	public Timetable getTimetable(String course, int year, String weeks)
+	{
+		List<Timetable> timetables = select(Timetable.class, "course=? AND year=? and weekRange=?", 
+				new String[]{course, String.valueOf(year), weeks}, 
+				null, "1");
+		if (timetables.isEmpty()) return null;
+		return timetables.get(0);
+	}
+	
+	public void saveTimetable(Timetable timetable)
+	{
+		replace(timetable);
+	}
 }
