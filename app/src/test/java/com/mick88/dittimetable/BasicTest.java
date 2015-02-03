6a8207592a741e437ed8780f2aa389ce41a2a5ad
michaldabski <contact@michaldabski.com>
2015-02-03 13:19:33 +0000

    Add Robolectric library

---
+++ app/src/test/java/com/mick88/dittimetable/BasicTest.java
@@ -0,0 +1,19 @@
+package com.mick88.dittimetable;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by Michal on 03/02/2015.
+ */
+@Config(emulateSdk = 18)
+public class BasicTest
+{
+    @Test
+    public void testName() throws Exception
+    {
+        Assert.assertFalse(false);
+    }
+}
